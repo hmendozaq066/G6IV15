f63609a11874f09e9b19c59e1f66c19d0cbc6711
udi.cecyt13@outlook.com <udi.cecyt13@outlook.com>
2022-02-23 18:26:12 -0600

    230202

---
+++ G6IV15/Models/Actividad.cs
@@ -0,0 +1,11 @@
+﻿//1.- poner los using's necesarios
+using System;
+namespace G6IV15.Models
+{
+    public class Actividad
+    {
+        public int ID { get; set; }
+        public string Descripcion { get; set; }
+        public int Estado { get; set; }
+    }
+}

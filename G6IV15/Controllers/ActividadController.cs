170ab7ccfe0205c56bc19cecc527a2e3cfb5807b
udi.cecyt13@outlook.com <udi.cecyt13@outlook.com>
2022-02-23 18:51:28 -0600

    23020202

---
+++ G6IV15/Controllers/ActividadController.cs
@@ -0,0 +1,20 @@
+﻿//1.- Agregar las librerias/ensamblados necesarias
+using Microsoft.AspNetCore.Mvc;
+using System;
+using G6IV15.Models;
+using System.Collections.Generic;
+
+namespace G6IV15.Controllers
+{
+    public class ActividadController : Controller
+    {
+        //Una instancia privada de tipo Lista, que va almacenar
+        //objetos de tipo Actividad
+        private List<Actividad> _actividades = new List<Actividad>();
+        public IActionResult Index()
+        {
+            ViewBag.Actividades = _actividades;
+            return View();
+        }
+    }
+}
